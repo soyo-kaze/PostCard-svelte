8169828c30d0ae0ee7e73a1bd77bf38a6b6892b0
soyo-kaze <07sameersaini@gmail.com>
2022-03-22 13:59:47 +0530

    Poster panel created with multiple uploads functionality

--- src/components/PosterPanel.svelte
+++ src/components/PosterPanel.svelte
@@ -1,15 +1,135 @@
 <script lang="ts">
   /**
-   * @todo Implement add__cards section and build cards switching section.
-   * @todo Implement image uploading functionality
+   * @todo Refactor the code using `svelte/stores` so that `ActionPanel` can do it's work.
+   * @todo Also shift all the JS Functions to a different file for better modularity and clean code.
    */
+  import { fly } from "svelte/transition";
+
+  interface imgProp {
+    imgSrc: string;
+    rotate: `${number}deg`;
+    scale: number;
+    key?: string;
+    opacity: string;
+    maxWidth: string;
+  }
+
+  let imgSrc: imgProp = {
+    imgSrc: "/assets/post.jpg",
+    rotate: "90deg",
+    scale: 0.55,
+    opacity: "1",
+    maxWidth: "120px",
+  };
+
+  let postCards: Array<imgProp> = [imgSrc];
+
+  /**
+   * It uses FileReader to read the file uploaded via file-type input tag and changes
+   * the image file to base64 string so it can be used in img tag as a source.
+   * @summary This Function is to upload and add image.
+   * @param {Event} e - It takes in an eventObject.
+   */
+
+  const handleUpload = (e: Event): void => {
+    // Typescript doesn't know about the type of this element so we assert it to the one we are confident about <HTMLInputElement>
+    const image = document.querySelector(
+      "input[type=file]"
+    ) as HTMLInputElement;
+
+    const file = image.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        function () {
+          // Convert image file to base64 string
+          let imgBase64 = reader.result;
+          let imgSrcInner = imgBase64 as string;
+
+          // Add multiple images to postCards array
+          postCards.push({
+            imgSrc: imgSrcInner,
+            rotate: "0deg",
+            scale: 1,
+            opacity: "0.5",
+            maxWidth: "100px",
+          });
+          postCards = postCards; // For Reactivity of svelte
+        },
+        false
+      );
+      reader.readAsDataURL(file);
+    }
+  };
+
+  /**
+   * We can change image from the small carousel to postCard panel where actions can be seen and updated.
+   * @summary switch between different postcards uploaded.
+   * @param {Event} e - Event Object.
+   * @param {imgProp} card - imgProp object contianing img data.
+   */
+
+  /**
+   * @todo use `key` for style updation and `filter()` method.
+   * @todo Use `active` class for changing the style of selected card.
+   */
+  const handleCard = (e: Event, card: imgProp) => {
+    if (card === imgSrc) return;
+
+    let img = e.target as HTMLImageElement;
+
+    let otherImgs = document.querySelectorAll(".small__card");
+    // Change style as per active tile
+    otherImgs.forEach((element) => {
+      (<HTMLImageElement>element).style.opacity = "0.5";
+      (<HTMLImageElement>element).style.maxWidth = "100px";
+    });
+
+    img.style.opacity = "1";
+    img.style.maxWidth = "120px";
+
+    imgSrc = card;
+  };
 </script>
 
 <main class="container__poster">
-  <div class="post__card">
-    <img src="./assets/post.jpg" alt="PostCard" />
+  {#key imgSrc}
+    <div
+      class="post__card"
+      in:fly={{ y: -200, delay: 300 }}
+      out:fly={{ y: 200, duration: 300 }}
+    >
+      <img
+        src={imgSrc.imgSrc}
+        alt="PostCard"
+        style={`rotate:${imgSrc.rotate};scale:${imgSrc.scale}`}
+      />
+    </div>
+  {/key}
+  <div class="add__cards">
+    <!--
+      Cards switching section 
+    -->
+    {#each postCards as card}
+      <img
+        src={card.imgSrc}
+        alt="SmallCards"
+        class="small__card"
+        style={`opacity:${card.opacity};max-width:${card.maxWidth}`}
+        on:click={(e) => handleCard(e, card)}
+      />
+    {/each}
+
+    <!--
+      Upload image section
+    -->
+    <form on:submit|preventDefault={(e) => handleUpload(e)}>
+      <input type="file" required accept="image/*" />
+      <button type="submit">Upload</button>
+    </form>
   </div>
-  <div class="add__cards">Add more cards</div>
 </main>
 
 <style>
@@ -40,8 +160,6 @@
     border-radius: 20px;
     max-width: 600px;
     width: 100%;
-    rotate: 0deg;
-    scale: 1;
   }
   .add__cards {
     padding: 20px 0px 10px 0px;
@@ -50,6 +168,19 @@
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  .small__card {
+    object-fit: contain;
+    border-radius: 10px;
+    transition-duration: 300ms;
+    width: 100%;
+    margin-left: 10px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    margin: 0px 10px 0px 10px;
+    max-width: 100px;
   }
   @media screen and (max-width: 750px) {
     .container__poster {
